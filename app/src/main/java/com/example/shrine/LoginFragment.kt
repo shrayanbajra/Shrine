795ece15cad6bffea8bf6942c46ccdf2882613d4
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-07-17 21:43:12 +0545

    LoginFragment -> Added authentication logic

--- app/src/main/java/com/example/shrine/LoginFragment.kt
+++ app/src/main/java/com/example/shrine/LoginFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginFragment : Fragment() {
 
@@ -12,8 +15,59 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
+    private lateinit var tilUsername: TextInputLayout
+    private lateinit var tilPassword: TextInputLayout
+
+    private lateinit var btnNext: MaterialButton
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tilUsername = view.findViewById(R.id.til_username)
+        tilPassword = view.findViewById(R.id.til_password)
+
+        btnNext = view.findViewById(R.id.btn_next)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnNextListener()
+
+    }
+
+    private fun btnNextListener() {
+        btnNext.setOnClickListener {
+
+            if (hasErrorEnabled(tilPassword)) clearError(tilPassword)
+
+            val password = getPassword()
+
+            if (isPasswordValid(password)) {
+
+                tilPassword.error = getString(R.string.password_error_message)
+
+            } else {
+
+                Toast.makeText(context, "Logging In", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+    }
+
+    private fun hasErrorEnabled(inputLayout: TextInputLayout) = inputLayout.isErrorEnabled
+
+    private fun clearError(inputLayout: TextInputLayout) {
+        inputLayout.isErrorEnabled = false
+    }
+
+    private fun getPassword() = tilPassword.editText?.text.toString()
+
+    private fun isPasswordValid(password: String) = password.length < 8
 
 }
