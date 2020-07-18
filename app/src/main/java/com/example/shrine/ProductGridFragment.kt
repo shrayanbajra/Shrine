a86ebcb3632bd138556d5c527f98a5731ad13ecf
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-07-18 21:42:21 +0545

    ProductGridFragment -> Added icons in Toolbar

--- app/src/main/java/com/example/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/example/shrine/ProductGridFragment.kt
@@ -1,9 +1,10 @@
 package com.example.shrine
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 
 class ProductGridFragment : Fragment() {
@@ -12,8 +13,49 @@ class ProductGridFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_grid, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_product_grid, container, false)
+        setToolbar(view)
+        return view
+    }
+
+    private fun setToolbar(view: View) {
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_product_grid_fragment, menu)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.item_search -> {
+                Toast.makeText(context, getString(R.string.title_search), Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.item_filter -> {
+                Toast.makeText(context, getString(R.string.title_filter), Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+
     }
 
 }
